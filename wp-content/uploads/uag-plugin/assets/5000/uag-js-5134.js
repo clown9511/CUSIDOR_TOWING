4826e40d21f43aa61f44f5c340d370bdc64bbd5a
Dayron Calderius Delgado <clown9511@gmail.com>
2025-06-14 23:30:35 -0400

    PrimerCommit

---
+++ wp-content/uploads/uag-plugin/assets/5000/uag-js-5134.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-d4ebb2b6' );
+});
+ });
---
+++ wp-content/wflogs/config-synced.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:19:{s:6:"apiKey";s:0:"";s:6:"isPaid";b:0;s:7:"siteURL";N;s:7:"homeURL";N;s:14:"whitelistedIPs";N;s:9:"howGetIPs";N;s:25:"howGetIPs_trusted_proxies";N;s:33:"howGetIPs_trusted_proxies_unified";N;s:13:"pluginABSPATH";N;s:11:"other_WFNet";b:1;s:9:"serverIPs";N;s:15:"blockCustomText";N;s:13:"timeoffset_wf";N;s:23:"advancedBlockingEnabled";s:1:"1";s:20:"disableWAFIPBlocking";s:1:"0";s:13:"patternBlocks";s:2:"[]";s:13:"countryBlocks";s:153:"{"blocks":[],"action":"block","loggedInBlocked":"","bypassRedirURL":"","bypassRedirDest":"","bypassViewURL":"","redirURL":"","cookieVal":"682e7e9e24608"}";s:11:"otherBlocks";s:33:"{"blockedTime":"300","blocks":[]}";s:8:"lockouts";s:37:"{"lockedOutTime":14400,"lockouts":[]}";}
